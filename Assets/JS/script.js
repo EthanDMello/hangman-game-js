0edd38c1069b72a07ce1a66c738fe4d60707f8db
EthanD'Mello <dmello.ethan.98@gmail.com>
2022-07-23 22:32:21 +0100

    added check inputs function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,5 @@
 words = "sesh";
+var letterAr = words.split("");
 letterDashBox = document.querySelector(".letterDash");
 
 function init() {
@@ -6,9 +7,9 @@ function init() {
   // reset hangman?
 }
 
-var button = document.querySelector("#startBtn");
 function startGame() {
   // get and set word
+  printWord();
   // start timer
   startTimer(60);
   // check for input
@@ -16,8 +17,8 @@ function startGame() {
 }
 
 function startTimer(seconds) {
+  // start timer and counts down
   var timerStart = setInterval(() => {
-    console.log(seconds);
     var timer = document.querySelector(".timer");
     timer.textContent = seconds;
     seconds--;
@@ -29,12 +30,42 @@ function startTimer(seconds) {
 }
 
 function printWord() {
+  // adds letter from as blanks to HTML body
   let letterAr = words.split("");
+  let letterId = 0;
+
   for (let i = 0; i < letterAr.length; i++) {
-    let letter = letterAr[i];
-    letterDashBox.appendChild(letter);
+    // loop creating individual sections with relative class and ids
+    // for the split word
+    el = document.createElement("section");
+    let classes = document.createAttribute("class");
+    classes.value = letterAr[i];
+    el.setAttributeNode(classes);
+    //
+    let ids = document.createAttribute("id");
+    ids.value = letterId;
+    el.setAttributeNode(ids);
+    letterId++;
+    //
+    el.textContent = "   _   ";
+    letterDashBox.appendChild(el);
   }
 }
 
-printWord();
+function checkInputs() {
+  document.addEventListener("keydown", keydownAction);
+  function keydownAction(event) {
+    for (let i = 0; i < letterAr.length; i++)
+      if (event.key == letterAr[i]) {
+        console.log("yes", letterAr[i]);
+        var class1 = document.querySelector(letterAr);
+        console.log(class1);
+        letterContent = "   " + letterAr[i] + "   ";
+        console.log(letterContent);
+        class1.textContent = letterContent;
+      }
+  }
+}
+var button = document.querySelector("#startBtn");
 button.addEventListener("click", startGame);
+checkInputs();
